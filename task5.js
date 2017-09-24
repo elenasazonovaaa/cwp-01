9868f2088e53ce58533e925e02a33e71bcd34586
elenasazonovaaa <elenasazonovaaa@gmail.com>
2017-09-24 19:32:59 +0300

    vse okey,no v sammary vnesti nado

--- task5.js
+++ task5.js
@@ -46,16 +46,42 @@ function createNewDirWithTXT(dirPath)
     fs.mkdir(dirPath+`\\`+nameDir,function (err) {
         if(err) console.log(`Err in create dir for TXT`);
     });
+    let config = "";
+    fs.readFile(`config.json`,function (err,data) {
+        if(err) console.log(`Err in read json`);
+        else
+        {
+            config = JSON.parse(data.toString());
+            moveTXT(dirPath,config,nameDir);
+        }
+    });
+
+}
+function moveTXT(dirPath,config,nameDir)
+{
     fs.readdir(dirPath,function (err,items) {
         if(err) console.log(`Err in seartch TXT`);
         else
         {
             for(let k = 0; k <items.length;k++)
             {
-                if(path.extname(items[k]) == `.txt`) fs.writeFile(
-                    dirPath+`\\`+nameDir+`\\`+items[k],``,
-                    function( err ) { if (err) console.log(`Err in move TXT`); }
-                )
+                if(path.extname(items[k]) === `.txt`)
+                {
+                    fs.readFile(dirPath+`\\`+items[k],function (err,data) {
+                        if(err) console.log(` It's bad` + err);
+                        else
+                        {
+                            fs.writeFile(
+                                dirPath+`\\`+nameDir+`\\`+items[k],config.copyright+data.toString()+config.copyright,
+                                function( err ) { if (err) console.log(`Err in move TXT`); }
+                            )
+                            fs.watch(dirPath,function (eventType,filename) {
+                                if(filename) console.log(filename.toString()+`---`+eventType.toString());
+                            })
+                        }
+                    });
+
+                }
             }
         }
     })
